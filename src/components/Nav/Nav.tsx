d66fe9860f6459af97a10db519b68b880b42d351
Chris Ware <c.p.ware@hotmail.com>
2025-02-11 21:11:48 -0700

    Nav Start

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,13 +1,42 @@
-"use state";
+"use client";
 
 import styles from "./Nav.module.css";
 import Logo from "../Logo/Logo";
-import Cart from "../../../public/icons/shoppingCart.svg";
 import Link from "next/link";
 import SearchBar from "../SearchBar/SearchBar";
-import NavbarIcons from "../NavbarIcons/NavbarIcons";
+// import NavbarIcons from "../NavbarIcons/NavbarIcons";
+import { useEffect, useState } from "react";
 
-export async function Nav() {
+export function Nav() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    if (body) {
+      if (window.innerWidth <= 968 && isOpen) {
+        body.style.overflow = "hidden";
+      } else {
+        body.style.overflow = "auto";
+      }
+    }
+
+    const handleResize = () => {
+      setIsOpen(false);
+      window.addEventListener("resize", handleResize);
+    };
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      if (body) {
+        body.style.overflow = "auto";
+      }
+    };
+  }, [isOpen]);
+
   const navItems = [
     {
       text: "Home",
@@ -49,17 +78,40 @@ export async function Nav() {
           <SearchBar />
         </div>
         <div className={styles.right}>
-          <ul className={styles.menu}>
-            {navItems.map((x, index) => (
-              <li className={styles.listItem} key={index}>
-                <Link href={x.href}>{x.text}</Link>
+          <ul
+            className={
+              isOpen === false
+                ? styles.navMenu
+                : `${styles.navMenu} ${styles.active}`
+            }
+          >
+            {navItems.map((navItem, index) => (
+              <li
+                key={index}
+                className={styles.navItem}
+                onClick={() => setIsOpen(false)}
+              >
+                <Link href={navItem.href} className={styles.navItem}>
+                  {navItem.text}
+                </Link>
               </li>
             ))}
+            {/* <div className={styles.navIconsContainer}>
+          <NavIcons color={color} />
+        </div> */}
           </ul>
-          <div className={styles.iconContainer}>
-            {/* <Cart className={styles.icon} /> */}
-            <NavbarIcons />
-          </div>
+          <span
+            className={
+              isOpen === false
+                ? styles.hamburger
+                : `${styles.hamburger} ${styles.active}`
+            }
+            onClick={openMenu}
+          >
+            <span className={styles.whiteBar}></span>
+            <span className={styles.whiteBar}></span>
+            <span className={styles.whiteBar}></span>
+          </span>
         </div>
       </nav>
     </header>
